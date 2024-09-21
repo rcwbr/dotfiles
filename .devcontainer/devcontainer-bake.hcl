895dd624c8eafc287bdb026cae30e5b6560d2da7
Eric Weber <github@weber.lol>
2024-09-21 15:55:42 +0000

    Pending changes exported from your codespace

--- .devcontainer/devcontainer-bake.hcl
+++ .devcontainer/devcontainer-bake.hcl
@@ -1,23 +1,12 @@
-variable "DEVCONTAINER_OUTPUTS" {
-  default = ""
-}
-
-group "default" {
-  targets = [
-    "pre-commit",
-    "useradd"
+variable "devcontainer_layers" {
+  default = [
+    "useradd",
+    "pre-commit"
   ]
-}
-
-target "pre-commit" {
-  contexts = {
-    base_context = "python:3.12.4"
-  }
 }
 
 target "useradd" {
   contexts = {
-    base_context = "target:pre-commit"
+    base_context = "docker-image://python:3.12.4"
   }
-  output = split(" ", trimspace("${DEVCONTAINER_OUTPUTS}"))
 }
--- install.sh
+++ install.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
 # Inspired by https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
-dir=dotfiles                                      # dotfiles directory
-olddir=~/dotfiles_old_$(date +%Y-%m-%d-%H-%M-%S)  # old dotfiles backup directory
+dotfiles="dotfiles"                                       # dotfiles directory
+home_dotfiles="$HOME/.dotfiles"                               # home dotfiles directory
+dotfiles_old="$HOME/.dotfiles_old_$(date +%Y-%m-%d-%H-%M-%S)"  # old dotfiles backup directory
 
 files=()                          # list of files/folders to symlink in homedir
 while IFS=  read -r -d $'\0'; do
   files+=("$REPLY")
-done < <(find dotfiles -maxdepth 1 -print0)
+done < <(find $dotfiles -maxdepth 1 -print0)
 
+echo "Files ${files}"
 
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+echo -n "Creating $dotfiles_old for backup of any existing dotfiles in $HOME ..."
+mkdir -p $dotfiles_old
 echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/$file $olddir/
+  echo "Moving any existing dotfiles from $HOME to $dotfiles_old"
+  mv $HOME/$file $dotfiles_old/ 
+done
+
+# copy the dotfiles directory to the homedir
+cp -r $dotfiles $home_dotfiles
+
+# change to the home_dotfiles directory
+echo -n "Changing to the $home_dotfiles directory ..."
+cd $home_dotfiles
+echo "done"
+
+# create symlinks from the homedir to any files in the $HOME/dotfiles directory specified in $files
+for file in $files; do
   echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/$file
+  ln -s $home_dotfiles/$file $HOME/$file
 done
