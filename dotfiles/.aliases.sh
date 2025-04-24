4839a6cec7d61eefee351301ebdc0c4288c6b004
Eric Weber <github@weber.lol>
2025-04-23 18:21:17 -0700

    fix: docker alias bash entrypoint argument

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -7,6 +7,6 @@ alias glf='git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)'
 alias gpu='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 
 # Custom Docker aliases
-alias drrieb="docker run --rm -it entrypoint bash"
+alias drrieb="docker run --rm -it --entrypoint bash"
 alias drri="docker run --rm -it"
 alias drr="docker run --rm"
