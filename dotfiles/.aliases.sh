e29beebd40485cf9c9aa8904d720254547c2e206
Eric Weber <github@weber.lol>
2025-06-22 17:05:57 +0000

    feat: add git and docker aliases

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Custom git aliases
+alias gcm="git commit -m "
+alias gkl="git clean -fd"
+alias gkld="git clean -n -fd"
 alias gl="git pull"
 alias gri="git rebase -i"
 alias gdo='git diff origin/$(git rev-parse --abbrev-ref HEAD)'
@@ -9,5 +12,6 @@ alias gpu='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 
 # Custom Docker aliases
 alias drrieb="docker run --rm -it --entrypoint bash"
+alias drrie="docker run --rm -it --entrypoint "
 alias drri="docker run --rm -it"
 alias drr="docker run --rm"
