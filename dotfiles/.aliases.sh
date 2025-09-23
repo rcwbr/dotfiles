867adbef49c469fdffd45b28123750f11adbc795
Eric Weber <github@weber.lol>
2025-09-23 01:13:18 +0000

    fix: docker alias workdir mount substitution

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -11,14 +11,22 @@ alias glf='git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)'
 alias gpu='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 
 # shellcheck disable=SC2016
-WORKING_DIR_MOUNT='-v ${PWD}:${PWD} -w ${PWD}'
+WORKING_DIR_MOUNT='-v $(pwd):$(pwd) -w $(pwd)'
 
 # Custom Docker aliases
-alias drrieb='docker run --rm -it --entrypoint bash'
-alias drriebw='docker run --rm -it ${WORKING_DIR_MOUNT} --entrypoint bash'
-alias drrie='docker run --rm -it --entrypoint '
-alias drriew='docker run --rm -it ${WORKING_DIR_MOUNT} --entrypoint '
-alias drri='docker run --rm -it'
-alias drriw='docker run --rm -it ${WORKING_DIR_MOUNT}'
-alias drr='docker run --rm'
-alias drrw='docker run --rm ${WORKING_DIR_MOUNT}'
+
+alias drrieb="docker run --rm -it --entrypoint bash"
+# shellcheck disable=SC2139
+alias drriebw="docker run --rm -it ${WORKING_DIR_MOUNT} --entrypoint bash"
+
+alias drrie="docker run --rm -it --entrypoint "
+# shellcheck disable=SC2139
+alias drriew="docker run --rm -it ${WORKING_DIR_MOUNT} --entrypoint "
+
+alias drri="docker run --rm -it"
+# shellcheck disable=SC2139
+alias drriw="docker run --rm -it ${WORKING_DIR_MOUNT}"
+
+alias drr="docker run --rm"
+# shellcheck disable=SC2139
+alias drrw="docker run --rm ${WORKING_DIR_MOUNT}"
